b788409a3a4e911ca827d9de1f63706b1da53332
Sasan <l.s.arham@gmail.com>
2023-10-29 21:14:05 +0330

    [Refactor] Remove commented configs from program.cs

--- WorkerProject/Program.cs
+++ WorkerProject/Program.cs
@@ -13,43 +13,6 @@ namespace WorkerProject
         {
             await CreateHostBuilder(args).Build().RunAsync();
         }
-
-
-        // for in memory: 
-        //public static IHostBuilder CreateHostBuilder(string[] args)
-        //{
-        //    Host.CreateDefaultBuilder(args)
-        //    .ConfigureServices((hostContext, services) =>
-        //    {
-        //        services.AddMassTransit(x =>
-        //        {
-        //            x.SetKebabCaseEndpointNameFormatter();
-
-        //            // By default, sagas are in-memory, but should be changed to a durable
-        //            // saga repository.
-        //            x.SetInMemorySagaRepositoryProvider();
-
-        //            var entryAssembly = Assembly.GetEntryAssembly();
-
-        //            x.AddConsumers(entryAssembly);
-        //            x.AddSagaStateMachines(entryAssembly);
-        //            x.AddSagas(entryAssembly);
-        //            x.AddActivities(entryAssembly);
-
-        //            x.UsingInMemory((context, cfg) =>
-        //            {
-        //                cfg.ConfigureEndpoints(context);
-        //            });
-        //        });
-
-        //        services.AddHostedService<Worker>();
-
-        //    });
-        //}
-
-
-
-
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
