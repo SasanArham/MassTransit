eeb235058aa72f90148a43eb44f8d0d967c57c90
Sasan <l.s.arham@gmail.com>
2023-10-29 21:39:45 +0330

    [Add] Implement WorkerConsumerDefinition

---
+++ WorkerProject/Consumer/WorkerConsumerDefinition.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+
+namespace WorkerProject.Consumer
+{
+    public class WorkerConsumerDefinition  :ConsumerDefinition<WorkerConsumer>
+    {
+        public WorkerConsumerDefinition()
+        {
+            // override the default endpoint name, for whatever reason
+            this.EndpointName = "whatever-name-you-want";
+
+            // limit the number of messages consumed concurrently
+            // this applies to the consumer only, not the endpoint
+            this.ConcurrentMessageLimit = 100;
+        }
+
+        protected override void ConfigureConsumer(IReceiveEndpointConfigurator endpointConfigurator, IConsumerConfigurator<WorkerConsumer> consumerConfigurator)
+        {
+            endpointConfigurator.UseMessageRetry(r => r.Interval(5, 1000));
+        }
+
+    }
+}
