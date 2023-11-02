aa67c61ac51902dca72f665ef62850382f683a0d
Sasan <l.s.arham@gmail.com>
2023-11-02 15:57:15 +0330

    [Fix] Fix rety policy configuration in WorkerConsumerDefinition

--- WorkerProject/Consumer/WorkerConsumerDefinition.cs
+++ WorkerProject/Consumer/WorkerConsumerDefinition.cs
@@ -19,29 +19,24 @@ namespace WorkerProject.Consumer
         protected override void ConfigureConsumer(IReceiveEndpointConfigurator endpointConfigurator, IConsumerConfigurator<WorkerConsumer> consumerConfigurator)
         {
             // It means only exceptions of specified types are going to include in rety policy
-            endpointConfigurator.UseMessageRetry(r => r.Handle<ArgumentException>());
-            endpointConfigurator.UseMessageRetry(r => r.Handle<NullReferenceException>());
-            endpointConfigurator.UseMessageRetry(r => r.Handle<SqlAlreadyFilledException>());
+            endpointConfigurator.UseMessageRetry(r => r.Interval(5, 1000).Handle<ArgumentException>());
+            endpointConfigurator.UseMessageRetry(r => r.Interval(1, 1000).Handle<NullReferenceException>());
 
-            endpointConfigurator.UseMessageRetry(r => r.Interval(5, 1000));
 
             // These are some other possible policies:
-
             //endpointConfigurator.UseMessageRetry(r => r.Immediate(5));
-
             //var initial = new TimeSpan(0, 1, 0);
             //var toAdd = new TimeSpan(0, 2, 0);
             //endpointConfigurator.UseMessageRetry(r => r.Incremental(5, initial, toAdd));
-
             //endpointConfigurator.UseMessageRetry(r => r.Interval(10, new TimeSpan(0, 10, 0)));
-
             //endpointConfigurator.UseMessageRetry(r => r.None());
 
 
 
             // It means if retry did not workd for ArgumentException , then try a second-level retry plicy in specified times
-            endpointConfigurator.UseDelayedRedelivery(r => r.Intervals(TimeSpan.FromHours(5),TimeSpan.FromHours(15), TimeSpan.FromHours(30)));
-            endpointConfigurator.UseDelayedRedelivery(r => r.Handle<ArgumentException>());
+            endpointConfigurator.UseDelayedRedelivery(r =>
+                r.Intervals(TimeSpan.FromHours(5), TimeSpan.FromHours(15), TimeSpan.FromHours(30))
+                .Handle<ArgumentException>());
 
         }
 
